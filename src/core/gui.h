fc25c648b96612c3ec93f4ca8fa3c9d49d8e9442
TcePrepK <arda.simsek@gmail.com>
2021-08-25 18:04:21 +0300

    .gitignore

--- src/core/gui.h
+++ src/core/gui.h
@@ -1,59 +1,53 @@
 #ifndef GUI_CPP
 #define GUI_CPP
 
-struct GUI
-{
-    s32 id;
-    u32 color;
-    Rect pos;
+struct GUI {
+	s32 id = 0;
+	u32 color = 0x000000;
+	Rect pos = { 0, 0, 0, 0 };
 };
 
-vector<GUI *> GUIs;
+vector<GUI*> GUIs;
 
 s32 w = 40;
 s32 h = 40;
 s32 off = 10;
 
-Rect bounds = {0, off, w, h};
+Rect bounds = { 0, off, w, h };
 
-void fixGUIs()
-{
-    s32 x = globalVariables.screenWidth - (w + off);
-    bounds = {x, off, x + w, off + h};
+void fixGUIs() {
+	s32 x = globalVariables.screenWidth - (w + off);
+	bounds = { x, off, x + w, off + h };
 
-    for (GUI *gui : GUIs)
-    {
-        gui->pos = {bounds.left, bounds.bottom - h, bounds.right, bounds.bottom};
-        bounds.bottom += h + off;
-    }
+	for (GUI* gui : GUIs) {
+		gui->pos = { bounds.left, bounds.bottom - h, bounds.right, bounds.bottom };
+		bounds.bottom += h + off;
+	}
 }
 
-void generateGUI(Element *e)
-{
-    GUI *gui = new GUI();
+void generateGUI(Element* e) {
+	GUI* gui = new GUI();
 
-    gui->id = e->id;
-    gui->color = e->color;
+	gui->id = e->id;
+	gui->color = e->color;
 
-    GUIs.push_back(gui);
+	GUIs.push_back(gui);
 
-    delete e;
+	delete e;
 }
 
-s32 testForGUIs()
-{
-    Vector m = globalVariables.mouse;
-    if (m.x < bounds.left || m.x > bounds.right || m.y < bounds.top || m.y > bounds.bottom)
-        return -1;
+s32 testForGUIs() {
+	Vector m = globalVariables.mouse;
+	if (m.x < bounds.left || m.x > bounds.right || m.y < bounds.top || m.y > bounds.bottom)
+		return -1;
 
-    for (s32 i = 0; i < GUIs.size(); i++)
-    {
-        GUI *gui = GUIs.at(i);
-        if (gui->pos.left <= m.x && m.x <= gui->pos.right && gui->pos.top <= m.y && m.y <= gui->pos.bottom)
-            return gui->id;
-    }
+	for (s32 i = 0; i < GUIs.size(); i++) {
+		GUI* gui = GUIs.at(i);
+		if (gui->pos.left <= m.x && m.x <= gui->pos.right && gui->pos.top <= m.y && m.y <= gui->pos.bottom)
+			return gui->id;
+	}
 
-    return globalVariables.currentMatID;
+	return globalVariables.currentMatID;
 }
 
 #endif // GUI_CPP
